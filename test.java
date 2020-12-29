e834425eeea569614e9dae0243cdd7aa26901a31
Аля Москвитина <10almoskv09@gmail.com>
2020-12-30 01:18:18 +0900

    .
    .

---
+++ test.java
@@ -0,0 +1,16 @@
+public class test {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.info();
+        triangle.perimeter();
+        triangle.square();
+        Rectangle rectangle = new Rectangle();
+        rectangle.info();
+        rectangle.perimeter();
+        rectangle.square();
+        intArray massiv = new intArray();
+        massiv.in();
+        massiv.out();
+        massiv.sort();
+    }
+}
